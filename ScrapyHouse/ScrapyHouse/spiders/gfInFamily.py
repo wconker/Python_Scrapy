7924d1ad141d40dfeef384744914a7774c271c7d
Your Nameconker <296491335@qq.com>
2019-02-12 13:39:15 +0800

    Default Changelist

---
+++ ScrapyHouse/ScrapyHouse/spiders/gfInFamily.py
@@ -0,0 +1,48 @@
+import scrapy
+import requests
+
+url = "http://web4.cartoonmad.com/home0256/4240/122/001.jpg"
+
+
+class DilidiliVideo(scrapy.Spider):
+    name = "gf"
+    box = [url]
+    index_page = 1
+    index_Charpest = 122
+    start_urls = box
+
+    def parse(self, response):
+        print("爬取开始....")
+        Contents = response.url
+        print(Contents.split('/')[6])
+
+        if self.index_page > 18:
+            self.index_page = 1
+            self.index_Charpest += 1
+            yield self.make_requests_from_url(
+                'http://web4.cartoonmad.com/home0256/4240/{0}/001.jpg'.format(self.pars2str(self.index_Charpest)))
+        else:
+            res = requests.get(Contents)
+
+            with open(self.pars2str(self.index_Charpest) + '00' + self.pars2str(self.index_page) + '.jpg', 'wb') as f:
+
+                f.write(res.content)
+
+            self.index_page += 1
+
+            print(self.index_page)
+
+            v_str = self.pars2str(self.index_page)
+
+            formatUrl = 'http://web4.cartoonmad.com/home0256/4240/' + self.pars2str(
+                self.index_Charpest) + '/{0}.jpg'.format(v_str)
+
+            yield self.make_requests_from_url(formatUrl)
+
+    def pars2str(self, num):
+        if 0 < num < 10:
+            return '00' + str(num)
+        if 9 < num < 100:
+            return '0' + str(num)
+        if num > 99:
+            return str(num)
